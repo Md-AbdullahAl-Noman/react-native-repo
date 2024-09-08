b83a5cbd21008930a76a4d59977bb3af42f2336e
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-08 20:58:12 +0600

    src added

---
+++ src/app/Index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Welcome to the src and inde</Text>
+    </View>
+  )
+}
+
+export default Index
